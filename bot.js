908d7a139e96dc78fce7522bee19cdddfbf88e17
chocoboii~ <63813281+defoko-bot@users.noreply.github.com>
2020-05-28 10:49:26 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,39 +53,46 @@ bot.on("message", async (message) => {
 	let tag = randomstuff[randomcategory];
 	let options = {
 	     url: "https://rule34.xxx/index.php?page=post&s=list&tags=" + tag,
-	     method: "GET",
+             method: "GET",
              headers: {
                  "Accept": "text/html",
-                 "User-Agent": "Chrome"
+                  "User-Agent": "Chrome"
              }
-        };
-     request(options, function(error, response, responseBody) {
-        if (error) {
-            // handle error
-            console.log(`EEEEEEE CARAIO ERRO EBAA: ` + error);
-        }
- 
-        /* Extract image URLs from responseBody using cheerio */
- 
-        $ = cheerio.load(responseBody); // load responseBody into cheerio (jQuery)
- 
-        // In this search engine they use ".image a.link" as their css selector for image links
-        var links = $(".image a.link");
- 
-        // We want to fetch the URLs not the DOM nodes, we do this with jQuery's .attr() function
-        // this line might be hard to understand but it goes thru all the links (DOM) and stores each url in an array called urls
-        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
-        console.log(urls);
-        if (!urls.length) {
-            message.channel.send(`nenhum hentaizera veio, tenta usar o cloudscraper ae men`)
-        }
- 
-        // Send result
-        message.channel.send( urls[0] );
-        message.channel.send( urls[0] );
-	message.channel.send( urls[0] );
-      });
+         };
+   request(options, function(error, response, responseBody) {
+    if (error) {
+     // handle error
+     return;
     }
+
+  // Extract image URLs from responseBody using cheerio 
+
+  $ = cheerio.load(responseBody); // load responseBody into cheerio (jQuery)
+
+  // In this search engine they use ".image a.link" as their css selector for image links 
+        // var links = $(".image a.link"); 
+        var allLinks = $("img");
+        var links = allLinks.splice(1, allLinks.length)
+
+  // We want to fetch the URLs not the DOM nodes, we do this with jQuery's .attr() function
+  // this line might be hard to understand but it goes thru all the links (DOM) and stores each url in an array called urls
+        var urls = new Array(links.length).fill(0).map((v, i) => links[i].attribs.src);
+        // var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+  console.log(urls);
+  if (!urls.length) {
+   // Handle no results
+   return;
+        }
+        
+        // Return a random image
+        var imageIndexToReturn = Math.floor(Math.random() * urls.length)
+
+  // Send result
+        message.channel.send( urls[imageIndexToReturn] );
+        
+ });
+
+}
     
     switch (args[0]) {
        case 'lol':

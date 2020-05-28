4c5fd163ddf321c3c512d72f9a64f1f9abd6bcfa
chocoboii~ <63813281+defoko-bot@users.noreply.github.com>
2020-05-28 10:38:53 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,38 +54,38 @@ bot.on("message", async (message) => {
 	let options = {
 	     url: "https://rule34.xxx/index.php?page=post&s=list&tags=" + tag,
 	     method: "GET",
-	     headers: {
-		 "Accept": "text/html",
-		 "User-Agent": "Chrome"
-	     }
-	};
-	request(options, function(error, response, responseBody) {
+             headers: {
+                 "Accept": "text/html",
+                 "User-Agent": "Chrome"
+             }
+        };
+     request(options, function(error, response, responseBody) {
         if (error) {
+            // handle error
             return;
         }
  
+        /* Extract image URLs from responseBody using cheerio */
  
-        $ = cheerio.load(responseBody);
+        $ = cheerio.load(responseBody); // load responseBody into cheerio (jQuery)
  
- 
+        // In this search engine they use ".image a.link" as their css selector for image links
         var links = $(".image a.link");
  
+        // We want to fetch the URLs not the DOM nodes, we do this with jQuery's .attr() function
+        // this line might be hard to understand but it goes thru all the links (DOM) and stores each url in an array called urls
         var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
-       
         console.log(urls);
- 
         if (!urls.length) {
-           
+            // Handle no results
             return;
         }
-        message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
-        message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
-        message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
-        message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
-        message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
-        message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
-	
-	});
+ 
+        // Send result
+        message.channel.send( urls[0] );
+        message.channel.send( urls[0] );
+	message.channel.send( urls[0] );
+      });
     }
     
     switch (args[0]) {
